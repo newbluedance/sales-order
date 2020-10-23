11af1f3c8fdfac5885b2e84b7955ae90fa895f27
lichunfeng <549434519@qq.com>
2020-10-23 11:14:31 +0800

    forth

--- src/main/java/com/licf/bgManage/service/impl/BgBannerServiceImpl.java
+++ src/main/java/com/licf/bgManage/service/impl/BgBannerServiceImpl.java
@@ -4,7 +4,10 @@ import com.common.base.BaseServiceImpl;
 import com.licf.bgManage.entity.BgBanner;
 import com.licf.bgManage.entity.dto.BgBannerParam;
 import com.licf.bgManage.entity.dto.BgBannerResult;
+import com.licf.bgManage.mapper.BgBannerMapper;
+import com.licf.bgManage.mapperstruct.BgBannerConverter;
 import com.licf.bgManage.service.BgBannerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,5 +17,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BgBannerServiceImpl extends BaseServiceImpl<BgBanner, BgBannerParam, BgBannerResult> implements BgBannerService {
-
+    @Autowired
+    public void init(BgBannerMapper bgBannerMapper, BgBannerConverter bgBannerConverter){
+        super.initMapperConverter(bgBannerMapper, bgBannerConverter);
+    }
 }
